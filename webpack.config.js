e8ed866035c1bd716189a689facb4e92dd911b40
nikolsky12@gmail.com <a7723556>
2019-09-04 23:44:26 +0300

    Fix

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require("path");
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const CopyPlugin = require('copy-webpack-plugin')ж
+const CopyPlugin = require('copy-webpack-plugin');
 
 module.exports = {
     entry: "./src/index.js",
